bfe61362d9ecd65410a772ef22b29d29ff2df7e1
Arne Chrestin <arne.chrestin@telekom.de>
2019-11-27 14:12:26 +0100

    Updated Player logic

--- src/com/wilders/blackjack/Player.java
+++ src/com/wilders/blackjack/Player.java
@@ -4,22 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player extends Gamer {
-
-
-	private int askAceValue() {
-		// to be done
-		return 1;
+    
+	
+	private boolean askNewCard() {
+		// To Do: Abfrage, ob der Player eine weitere Karte ziehen möchte
+		return false;
 	}
 	
 	public int logic(List<Card> deck) {
-		// Draw two cards for the player and show them
+
 		List<Card> hand = this.getHand();
+        List<Card> aceCards = new ArrayList<Card>();
+		// Draw initial two cards for the player
+        // Draw first card:
 		hand.add(deck.remove(0));
-		hand.get(0).setVisibility(true);
-		hand.add(deck.remove(0));
+        hand.get(0).setVisibility(true);
+        if (hand.get(0).isAce()) aceCards.add(hand.get(0));
+		// Draw second card:
+        hand.add(deck.remove(0));
 		hand.get(1).setVisibility(true);
-		this.showHand();
-		while ()
+        if (hand.get(1).isAce()) aceCards.add(hand.get(1));
+		// Show cards and ask for ace value if applicable
+        this.showHand();
+        if (aceCards != null) this.setAceValue(aceCards);
+        aceCards.removeAll(aceCards);
+        // Draw further cards if player wants
+		while (this.getTotalValue() < 21) {
+			if (askNewCard()) {
+				hand.add(deck.remove(0));
+				
+			}
+		}
 		return 0;
 	}
 
