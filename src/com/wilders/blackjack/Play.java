6eecd43a627160a893ad603df320e8dcda7aa0ed
Michael Blisner <Michael.Blisner@telekom.de>
2019-11-27 16:58:51 +0100

    deck in Card verortet

--- src/com/wilders/blackjack/Play.java
+++ src/com/wilders/blackjack/Play.java
@@ -7,42 +7,7 @@ import java.util.Scanner;
 
 public class Play {
 	
-	private static List<Card> initDeck() {
-		List<Card> deck = new ArrayList<>();
-		// mit Karten füllen und mischen
-
-		for (int i = 2; i <= 10; i++) {
-			deck.add(new Card("diamond", Integer.toString(i), i, false));
-			deck.add(new Card("heart", Integer.toString(i), i, false));
-			deck.add(new Card("spade", Integer.toString(i), i, false));
-			deck.add(new Card("cross", Integer.toString(i), i, false));
-			
-		}
-		deck.add(new Card("diamond", "J", 10, false));
-		deck.add(new Card("diamond", "Q", 10, false));
-		deck.add(new Card("diamond", "K", 10, false));
-		deck.add(new Card("diamond", "Ace", 11, false));
-				
-		deck.add(new Card("heart", "J", 10, false));
-		deck.add(new Card("heart", "Q", 10, false));
-		deck.add(new Card("heart", "K", 10, false));
-		deck.add(new Card("heart", "Ace", 11, false));
-		
-		deck.add(new Card("spade", "J", 10, false));
-		deck.add(new Card("spade", "Q", 10, false));
-		deck.add(new Card("spade", "K", 10, false));
-		deck.add(new Card("spade", "Ace", 11, false));
-						
-		deck.add(new Card("cross", "J", 10, false));
-		deck.add(new Card("cross", "Q", 10, false));
-		deck.add(new Card("cross", "K", 10, false));
-		deck.add(new Card("cross", "Ace", 11, false));
-
-		//mix
-		Collections.shuffle(deck);
-				
-		return deck;
-	}
+	
 
 	private static boolean askContinue() {
 		Scanner s = new Scanner(System.in);
@@ -61,7 +26,7 @@ public class Play {
 		Dealer dealer = new Dealer();
 
 		do {
-			List deck = initDeck();
+			List deck = Card.initDeck();
 			player.cleanHand();
 			dealer.cleanHand();
 			int playerValue = player.logic(deck);
